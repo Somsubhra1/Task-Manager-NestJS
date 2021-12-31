6bec1c92c9f6835255acfdfd7d17c01e861d6fef
Somsubhra Das <das.somsubhra1@gmail.com>
2022-01-01 03:29:12 +0530

    task repository imported

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,10 +2,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
 import { TasksController } from "./tasks.controller";
 import { TasksService } from "./tasks.service";
-import { Task } from "./task.entity";
+import { TasksRepository } from "./tasks.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   controllers: [TasksController],
   providers: [TasksService],
 })
