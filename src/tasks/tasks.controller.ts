5598ee47ed9732da1052920df32c8da8731e1177
Somsubhra Das <das.somsubhra1@gmail.com>
2021-12-31 23:10:09 +0530

    feat: get tasks and create task done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Param, Post } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { Task } from "./task.entity";
 
 @Controller("tasks")
-export class TasksController {}
+export class TasksController {
+  private logger = new Logger("TaskController");
+  constructor(private readonly taskService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    this.logger.log("Get tasks");
+    return this.taskService.getTasks();
+  }
+
+  @Post()
+  createTask(@Body() task: Task) {
+    this.logger.debug("Creating task");
+    return this.taskService.createTask(task);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,35 @@
+import { Task } from "./task.entity";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private readonly tasks: Task[] = [
+    {
+      title: "Test 1",
+      description: "Desc 1",
+      isCompleted: false,
+      taskDate: new Date(),
+      id: 1,
+    },
+    {
+      title: "Test 2",
+      description: "Desc 2",
+      isCompleted: false,
+      taskDate: new Date(),
+      id: 1,
+    },
+  ];
+  getTasks() {
+    return this.tasks;
+  }
+
+  createTask(task: Task) {
+    task.isCompleted = false;
+    task.taskDate = new Date();
+    task.id = this.tasks.length + 1;
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
