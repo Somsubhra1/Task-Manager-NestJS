a5bc8197800cac3dba92b708f989c050a05eed45
Somsubhra Das <das.somsubhra1@gmail.com>
2022-01-01 11:01:00 +0530

    add: user repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
+import { UserRepository } from "./user.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UserController],
   providers: [UserService],
 })
